a84ffdfad2e5873a72908d1641a9a359c4072fa6
georgejiang97 <georgjia@umich.edu>
2018-03-07 15:05:30 -0500

    done

--- rpn.py
+++ rpn.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import operator
-import requests
-import json
+# import requests
 
 operators = {
     '+': operator.add,
@@ -28,29 +27,29 @@ def calculate(myarg):
     return stack.pop()
 
 def main():
-    param = {
-        "jsonrpc": "2.0",
-        "method": "generateIntegers",
-        "params": {
-            "apiKey": "ea3725d4-270b-4f2d-83ab-9d80e30aac81",
-            "n": 2,
-            "min": 0,
-            "max": 100,
-            "replacement": True
-        },
-        "id": 3405130
-    }
-    r = requests.post('https://api.random.org/json-rpc/1/invoke', json=param)
-    print "Your random numbers for today are ", json.loads(r.text)['result']['random']['data']
+    # param = {
+    #     "jsonrpc": "2.0",
+    #     "method": "generateIntegers",
+    #     "params": {
+    #         "apiKey": "ea3725d4-270b-4f2d-83ab-9d80e30aac81",
+    #         "n": 2,
+    #         "min": 0,
+    #         "max": 100,
+    #         "replacement": True
+    #     },
+    #     "id": 3405130
+    # }
+    # r = requests.post('https://api.random.org/json-rpc/1/invoke', json=param)
+    # print "Your random numbers for today are ", json.loads(r.text)['result']['random']['data']
     while True:
         result = calculate(input("rpn calc> "))
         print("Result: ", result)
 
-    print("hi1")
-    print("hi2")
-    print("hi3")
-    a = 2 * 2
-    print(a)
+    # print("hi1")
+    # print("hi2")
+    # print("hi3")
+    # a = 2 * 2
+    # print(a)
 
 if __name__ == '__main__':
     main()
